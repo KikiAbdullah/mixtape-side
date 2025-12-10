f0288858e59624b163860e1c641d13894d1bf47e
kikiAbdullah <kikirabdullah@gmail.com>
2025-12-10 14:21:39 +0700

    remove DataSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([UserSeeder::class, PermissionSeeder::class, DataSeeder::class]);
+        $this->call([UserSeeder::class, PermissionSeeder::class]);
     }
 }
